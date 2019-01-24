0646f1da69501a8f51f827e591bdebe15f517fcb
drpeabody <abhiagar97@gmail.com>
2019-01-24 14:33:09 +0530

    Got Mongo Working

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 
-const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 const mongod = require('mongod');
 // const exec = require('child_process').exec;
 const mongo_url = "mongodb://localhost:27017";
+const db_name = 'lambda';
 const vm = require('vm');
 const stdin = process.openStdin();
-var db = undefined;
-// console.log('Yolo');
+var db_client;
+var db;
 const server = new mongod({ port: 27017, dbpath: 'data' });
-// console.log('uded');
 
 startup = () => {
 	console.log('Attempting to Start Mongo Server');
 
 	process.on('exit', () => {
-		if(!db) {
+		if(!db_client) {
 			console.log('Closing Mongo DB...');
-			db.close();
+			db_client.close();
 		}
 	});
 
@@ -30,25 +30,31 @@ startup = () => {
 		else console.log('Mongo Server Started');
 	});
 
-	mongodb.connect(mongo_url, { useNewUrlParser: true }, function(err) {
-		if (err) console.log(err);
-		console.log("Database connected@" + mongo_url);
-		db = mongodb.MongoClient.db('lambda');
-		console.log("Database contains " + db.find({}).toArray().length)
+	db_client = new MongoClient(mongo_url, { useNewUrlParser: true });
+	db_client.connect((err) => {
+		if(err) console.log(err);
+		else {
+			console.log('MongoDB connected to:' + mongo_url + '/' + db_name);
+			db = db_client.db(db_name);
+		}
 	});
+	
 	console.log('Server Started');
+
+	stdin.addListener("data", function(d) {
+		var g = String(d).trim();
+		try {
+			console.log(eval(g));	
+		} catch(err) {
+			console.log(err);
+		}
+		console.log('>\b');
+	});
+
 }
 
 startup();
 
-stdin.addListener("data", function(d) {
-	var g = String(d).trim();
-	try {
-		console.log(eval(g));	
-	} catch(err) {
-		console.log(err);
-	}
-});
 
 
 
