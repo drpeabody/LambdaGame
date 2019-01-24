2c4c9d569dc689ca71c3d01e401324471b26235a
drpeabody <abhiagar97@gmail.com>
2019-01-24 22:49:41 +0530

    Wrote helper functions for MongoDB

--- server.js
+++ server.js
@@ -10,6 +10,45 @@ var db_client;
 var db;
 const server = new mongod({ port: 27017, dbpath: 'data' });
 
+mongoInsertOne = (collection, doc, callback) => {
+	db.collection(collection).insertOne(doc, function(err, result) {
+		if(!err) {
+			console.log("Inserted document(s): ", doc);
+			if(callback) callback(result);
+		}
+		else console.log(err);
+	});
+}
+mongoFind = (collection, doc, callback) => {
+	db.collection(collection).find(doc).toArray(function(err, docs) {
+		if(!err) {
+			console.log("Found "+docs.length+" document(s).");
+			if(callback) callback(docs);
+		}
+		else console.log(err);
+	});
+}
+mongoUpdateOne = (collection, query, modifier, callback) => {
+  	db.collection(collection).updateOne(query, modifier, function(err, result) {
+		if(!err) {
+			console.log("Matched " + result.matchedCount + ", modified " + result.modifiedCount + " documents.");
+			if(callback) callback(result);
+		}
+		else console.log(err);
+  	});  
+}
+mongoDeleteOne = function(collection, query, callback) {
+  	db.collection(collection).deleteOne(query, function(err, result) {
+		if(!err) {
+			console.log("Removed " + result.result.n + " documents.");
+			if(callback) callback(result);
+		}
+		else console.log(err);
+  	});    
+}
+
+
+
 startup = () => {
 	console.log('Attempting to Start Mongo Server');
 
@@ -28,28 +67,29 @@ startup = () => {
 			else console.log('Error while Starting Mongo Server:\n'+err);
 		}
 		else console.log('Mongo Server Started');
+
+		db_client = new MongoClient(mongo_url, { useNewUrlParser: true });
+		db_client.connect((err) => {
+			if(err) console.log(err);
+			else {
+				console.log('MongoDB connected to:' + mongo_url + '/' + db_name);
+				db = db_client.db(db_name);
+			}
+		});
 	});
 
-	db_client = new MongoClient(mongo_url, { useNewUrlParser: true });
-	db_client.connect((err) => {
-		if(err) console.log(err);
-		else {
-			console.log('MongoDB connected to:' + mongo_url + '/' + db_name);
-			db = db_client.db(db_name);
-		}
-	});
-	
-	console.log('Server Started');
 
 	stdin.addListener("data", function(d) {
 		var g = String(d).trim();
+		if(g === 'exit') process.exit();
 		try {
 			console.log(eval(g));	
 		} catch(err) {
 			console.log(err);
 		}
-		console.log('>\b');
 	});
+
+	console.log('Server Started');
 
 }
 
