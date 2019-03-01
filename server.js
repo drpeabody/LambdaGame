0ba2365d9a6ab3675f160e628ec264b2203f9705
adaephonben <ma18btech11011@iith.ac.in>
2019-03-01 21:57:03 +0530

    Fixed mouse attack issue

--- server.js
+++ server.js
@@ -232,7 +232,6 @@ startup = () => {
                         },11);          
                     }
                 }
-                player.mouseDown = false ;
             }
             if (userArray[ID-1].health <= 0)
             {
